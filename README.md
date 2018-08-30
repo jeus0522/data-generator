2eb6e0929416f17f7efc3b081d6745a59446fcfd
jeus0522 <tiger020522@outlook.com>
2018-08-29 23:33:18 -0300

    Finished linear regression data generator

---
+++ README.md
@@ -0,0 +1,23 @@
+# Data generator for machine learning models
+
+This repository is meant to be a collection of scripts for generating 
+different data sets that can be used to train machine learning models.
+
+This data points are randomly generated and try to imitate real world data.
+They can be used to test how a model performs on different data 
+distributions for the same task.
+
+## Available generators
+
+### [Linear regression](https://github.com/fiquinho/data-generator/tree/master/linear_regression)
+
+This module will create a data set of 2D data points that can be model 
+using linear regression. They will be created by randomly deviating from 
+a linear function. The linear function parameters, and the distribution 
+of the deviations can be controlled by the script. It will generate a 
+.csv file with the data, and it will plot the generated data. The linear 
+function and the deviations distribution can also be plotted.
+
+The plotted data will look something like this:
+
+![linear_regression_data_example](linear_regression/plot_example.png)
--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -6,10 +6,6 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-
 def linear_function(x: np.array, w: float=None, b: float=None) -> np.array:
     """
     Apply a linear function to all the values in a numpy array.
@@ -25,26 +21,36 @@ def linear_function(x: np.array, w: float=None, b: float=None) -> np.array:
 
 
 def generate_linear_data(data_size: int=20, mu: float=0, sigma: float=1.0,
-                         weight: float=np.random.uniform(low=0, high=1),
-                         bias: float=np.random.uniform(low=-1, high=1),
+                         weight: float=None, bias: float=None,
                          x_limits: tuple=(1, 10), deviations_histogram: bool=False,
-                         plot_line: bool=False):
+                         plot_line: bool=False, seed: int=None, output: Path=None):
     """
     Create data points that are randomly deviated from a linear function. The deviations
     come from a normal distribution that is configured in the parameters. The points
     are uniformly distributed on the x axis in the specified range. The data points
     are plotted to a graph. The generated data is saved to a .csv file with headers.
 
-    :param data_size: The number of data points to create
-    :param mu: The mean of the deviations normal distribution
-    :param sigma: The standard deviation of the deviations normal distribution
-    :param weight: The slope of the linear function
-    :param bias: Intersection point of the linear function with the y axis
-    :param x_limits: A tuple with the range of the generated x values
-    :param deviations_histogram: Activate to plot the deviations histogram
-    :param plot_line: Activate to plot the linear function with the data points
+    :param data_size: The number of data points to create.
+    :param mu: The mean of the deviations normal distribution.
+    :param sigma: The standard deviation of the deviations normal distribution.
+    :param weight: The slope of the linear function.
+    :param bias: Intersection point of the linear function with the y axis.
+    :param x_limits: A tuple with the range of the generated x values.
+    :param deviations_histogram: Activate to plot the deviations histogram.
+    :param plot_line: Activate to plot the linear function with the data points.
+    :param seed: The Numpy seed to use on the random numbers generation.
+    :param output: Where to save the generated data file. Defaults to this script folder.
     :return: None
     """
+
+    # Fixing random state for reproducibility
+    if seed:
+        np.random.seed(seed)
+
+    if not weight:
+        weight = np.random.uniform(low=0, high=1)
+    if not bias:
+        bias = np.random.uniform(low=-1, high=1)
 
     # Create the graph figures
     if deviations_histogram:
@@ -60,8 +66,8 @@ def generate_linear_data(data_size: int=20, mu: float=0, sigma: float=1.0,
     for i in range(data_size):
         x.append(x_limits[0] + (x_range / data_size) * i)
 
-    print("Initial weight = {}".format(weight))
-    print("Initial bias = {}".format(bias))
+    print("Line weight = {}".format(weight))
+    print("Line bias = {}".format(bias))
 
     # Generate random deviations from a normal distribution N(mu, sigma^2)
     deviations = np.random.randn(data_size) * sigma + mu
@@ -73,7 +79,7 @@ def generate_linear_data(data_size: int=20, mu: float=0, sigma: float=1.0,
     # Plot the data
     plt.axis("equal")
     plt.title("Data points")
-    plt.plot(x, y, 'o')
+    plt.plot(x, y, 'o', markersize=5)
 
     # Plot the line with the data points
     if plot_line:
@@ -83,13 +89,18 @@ def generate_linear_data(data_size: int=20, mu: float=0, sigma: float=1.0,
         line_x_max = x_limits[1] + x_range * line_extra_space
         line_range = line_x_max - line_x_min
         for i in range(data_size):
-            x_line.append(line_x_min + (line_range / data_size) * i )
+            x_line.append(line_x_min + (line_range / data_size) * i)
 
         y_line = [value * weight + bias for value in x_line]
 
+        if bias < 0:
+            bias_text = " - {}".format(abs(round(bias, 3)))
+        else:
+            bias_text = " + {}".format(round(bias, 3))
+
         plt.text(x_line[0], y_line[-1],
                  "Linear function:\n"
-                 "x * {} + {}".format(round(weight, 3), round(bias, 3)),
+                 "x * {}".format(round(weight, 3), round(bias, 3)) + bias_text,
                  fontweight='bold',
                  bbox={"facecolor": "orange", "alpha": 0.5, "pad": 5})
         plt.plot(x_line, y_line)
@@ -110,7 +121,7 @@ def generate_linear_data(data_size: int=20, mu: float=0, sigma: float=1.0,
 
         plt.hist(data, bins=35)
         plt.title("Deviations histogram")
-        # TODO: Plot mu and sigma as symbols in the graph
+
         plt.text(sigma * -4, 0.08 * data.shape[0],
                  "Normal distribution:\n"
                  "mu = {}\nsigma = {}".format(round(mu, 3), round(sigma, 3)),
@@ -119,27 +130,30 @@ def generate_linear_data(data_size: int=20, mu: float=0, sigma: float=1.0,
 
         plt.grid()
 
-    # TODO: Continue from here
-    data_file = Path("data.csv")
+    # Delete old data file
+    if not output:
+        output = Path(__file__).parent
+    data_file = Path(output, "linear_data.csv")
     if data_file.exists():
         data_file.unlink()
 
-    with open("data.csv", 'a', newline='') as csvfile:
+    # Save generated data to a csv file
+    with open(data_file, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
         writer.writerow(["x", "y"])
         for i in tqdm(range(len(x))):
             writer.writerow([x[i], y[i]])
 
-
-
     plt.show()
 
 
 def main():
 
-    generate_linear_data(data_size=10,
-                         deviations_histogram=True, plot_line=True)
+    generate_linear_data(data_size=10, x_limits=(1, 10), sigma=1,
+                         deviations_histogram=True, plot_line=True,
+                         seed=19680801)
+
 
 if __name__ == '__main__':
     main()

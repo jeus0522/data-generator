b19fe634c3ea4d09be6eefd105345347025dc507
jeus0522 <tiger020522@outlook.com>
2022-07-13 04:41:39 -0300

    Added linear regression link

--- Data_Generator_APP.py
+++ Data_Generator_APP.py
@@ -6,3 +6,4 @@ st.sidebar.markdown("# Data generators")
 
 st.markdown("# Available generators")
 
+st.markdown(" - [Linear Regression](https://fiquinho-data-generator-data-generator-app-yo8n6m.streamlitapp.com/Linear_Regression)")
